b426656297207331a679c120e955a0521c502234
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-02-02 09:46:50 -0300

    update

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
+
+const flight = 'LH123';
+const jonas = {
+  name: 'jonas Scream',
+  passport: 1254636656,
+};
+
+const checkIn = function (flighNum, passenger) {
+  flighNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 1254636656) {
+    alert('checked in');
+  } else {
+    alert('wrong passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const flighNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
