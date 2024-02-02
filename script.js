f8744e9588ec26dba253aeb8a3b6b8d503948311
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-02-02 09:53:11 -0300

    .

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
-*/
+
 
 const flight = 'LH123';
 const jonas = {
@@ -53,3 +53,4 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
